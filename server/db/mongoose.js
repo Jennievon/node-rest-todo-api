128d3648b206ee31ab5b26015c46b676d6faa843
Jennievon <echenimjennifer@gmail.com>
2021-08-02 11:53:30 +0100

    update MONGODB_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp"
+	process.env.MONGODB_URI ||
+		"mongodb+srv://von:odins108@cluster0.lcvfs.mongodb.net/todo?retryWrites=true&w=majority",
 );
 
 module.exports = { mongoose };
